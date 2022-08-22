79cd23416714a09ef10a66b6eff7eb931aa97af2
2572636146 <111727111+2572636146@users.noreply.github.com>
2022-08-23 01:34:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app_id = 'wx1c7fea5c1735db5e'
 app_secret = '42af3a400bdd73588ee8c31372211510'
 user_id = 'oyJTp54JOJgoWwPtbJ9gHzs6lfrw'
 
-template_id = 'lRJGq9bWA6-77NtdVvVYnM2s2nLL_MSmY8etUz6aEXs'
+template_id = 'J4fC2diITpHTWhONtJBfvVaRDajkNGgJELW2AtTL87E'
 
 
 def get_weather():
